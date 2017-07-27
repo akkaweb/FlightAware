f54ae418e07d6fc5f0b5d56d715aa8584a3c5b18
Kai van Rijswijk <support@belimitless.nl>
2017-07-27 13:01:06 +0200

    Update demo.php

--- demo.php
+++ demo.php
@@ -4,7 +4,7 @@
     include('flightAware.php');
     
     // Initiate class
-    $flightAware = new FlightAware('#YOUR_FLIGHT_AWARE#', '#YOUR_FLIGHT_AWARE_API_KEY#');
+    $flightAware = new FlightAware('#YOUR_FLIGHT_AWARE_USERNAME#', '#YOUR_FLIGHT_AWARE_API_KEY#');
     
     // Pull data from FlghtAware
     $scheduledFleet = $flightAware->pull('FleetScheduled', [ 'fleet' => 'KLM' ] );
