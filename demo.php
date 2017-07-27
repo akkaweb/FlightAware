8399756f374722a69c0bd95d6046a0ad9e469e0f
Kai van Rijswijk <support@belimitless.nl>
2017-07-27 13:00:29 +0200

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,13 @@
+<?php
+    
+    // Include the FlightAware class
+    include('flightAware.php');
+    
+    // Initiate class
+    $flightAware = new FlightAware('#YOUR_FLIGHT_AWARE#', '#YOUR_FLIGHT_AWARE_API_KEY#');
+    
+    // Pull data from FlghtAware
+    $scheduledFleet = $flightAware->pull('FleetScheduled', [ 'fleet' => 'KLM' ] );
+    
+    // Output data
+    echo "<pre>"; print_r( $scheduledFleet );
